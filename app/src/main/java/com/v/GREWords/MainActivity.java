9673ea16d1bc2c3e668cab5795bb65ac99fd4f85
vjFaLk <valmik2k2@gmail.com>
2014-03-17 13:45:13 +0530

    Back Button and Effects

--- app/src/main/java/com/v/GREWords/MainActivity.java
+++ app/src/main/java/com/v/GREWords/MainActivity.java
@@ -6,25 +6,30 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.BounceInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
 
-    String definition = "";
+    String definition = "", word = "";
     TextView mainText, defText;
-    Button nextBut;
+    int currentIndex =-1;
+    Button nextBut, prevBut;
     boolean shownDef = false;
+    boolean shownFade = false;
+    List wordList = new ArrayList();
+    List defList = new ArrayList();
+
+
 
 
     @Override
@@ -32,26 +37,67 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        changeWord();
+        Initialize();
         setActionListeners();
-
+        newWord();
 
 
     }
 
-    private void setActionListeners()
+
+
+    private void Initialize()
     {
-        nextBut = (Button)findViewById(R.id.button);
+        defText = (TextView) findViewById(R.id.textView2);
+        mainText = (TextView) findViewById(R.id.textView);
+        nextBut = (Button) findViewById(R.id.buttonNext);
+        prevBut = (Button) findViewById(R.id.buttonPrev);
+        prevBut.setVisibility(View.GONE);
+
+    }
+
+    private void setActionListeners() {
+
+
+
         nextBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(shownDef==true)
-                {
-                findViewById(R.id.textView).clearAnimation();
+
+                Animation in = new AlphaAnimation(0.0f, 1.0f);
+                in.setDuration(200);
+
+
+                if (shownDef) {
+                    findViewById(R.id.textView).clearAnimation();
                     defText.setText(" ");
                 }
 
-                changeWord();
+
+                if(!shownFade)
+                {
+                    prevBut.setVisibility(View.VISIBLE);
+                    prevBut.setAnimation(in);
+                    shownFade = true;
+                }
+
+                if(((wordList.size())-1)>currentIndex)
+                getWordFromList();
+                else
+                newWord();
+
+
+
+
+            }
+        });
+
+        prevBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(currentIndex>0)
+                goBack();
+
 
             }
         });
@@ -59,23 +105,102 @@ public class MainActivity extends Activity {
         mainText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(shownDef==false)
-                showDefinition();
+                if (shownDef == false)
+                    showDefinition();
+                else
+                    hideDefinition();
             }
         });
 
 
     }
 
+    private void hideDefinition() {
+        Animation out = new AlphaAnimation(1.0f, 0.0f);
+        out.setDuration(500);
+        defText.setVisibility(View.GONE);
+        defText.setAnimation(out);
+        TranslateAnimation translation;
+        translation = new TranslateAnimation(0f, 0f, getDisplayHeight()-350, 0f);
+        translation.setStartOffset(0);
+        translation.setDuration(500);
+        translation.setFillAfter(true);
+        translation.setInterpolator(new DecelerateInterpolator());
+        findViewById(R.id.textView).startAnimation(translation);
+        shownDef = false;
+    }
+
+    private void showDefinition() {
+
+        Animation in = new AlphaAnimation(0.0f, 1.0f);
+        in.setDuration(500);
+        findViewById(R.id.textView).clearAnimation();
+        defText.setVisibility(View.VISIBLE);
+        TranslateAnimation translation;
+        translation = new TranslateAnimation(0f, 0F, 0f, getDisplayHeight() - 350);
+        translation.setStartOffset(0);
+        translation.setDuration(500);
+        translation.setFillAfter(true);
+        translation.setInterpolator(new DecelerateInterpolator());
+        findViewById(R.id.textView).startAnimation(translation);
+
+        defText.setText(definition);
+        defText.startAnimation(in);
+        shownDef = true;
+
+    }
+
+    private void getWordFromList() {
+
+        currentIndex++;
+        Animation in = new AlphaAnimation(0.0f, 1.0f);
+        in.setDuration(200);
+        word = wordList.get(currentIndex).toString();
+        definition = defList.get(currentIndex).toString();
+        mainText.setText(word);
+        mainText.setAnimation(in);
+        shownDef = false;
 
 
-    private void changeWord() {
-        mainText = (TextView) findViewById(R.id.textView);
+    }
+
+    private boolean checkStack()
+    {
+        Animation out = new AlphaAnimation(1.0f, 0.0f);
+        out.setDuration(200);
+
+        if(currentIndex<1)
+        {
+            prevBut.setVisibility(View.GONE);
+            prevBut.setAnimation(out);
+            shownFade = false;
+            return false;
+        }
+        return true;
+    }
+
+    private void goBack()
+    {
+        currentIndex--;
+        Animation in = new AlphaAnimation(0.0f, 1.0f);
+        in.setDuration(200);
+        findViewById(R.id.textView).clearAnimation();
+        defText.setText(" ");
+        mainText.setText(wordList.get(currentIndex).toString());
+        mainText.setAnimation(in);
+        definition = defList.get(currentIndex).toString();
+        shownDef = false;
+        checkStack();
+
+    }
+
+
+    private void newWord() {
+
         DataBaseHelper data;
         data = new DataBaseHelper(this);
         Animation in = new AlphaAnimation(0.0f, 1.0f);
         in.setDuration(500);
-
 
 
 
@@ -88,44 +213,31 @@ public class MainActivity extends Activity {
         try {
 
             data.openDataBase();
-           mainText.setText(data.getWord());
+            word = data.getWord();
+            mainText.setText(word);
             mainText.setAnimation(in);
-           definition = data.getDefinition();
+            definition = data.getDefinition();
         } catch (SQLException sqle) {
 
             throw sqle;
         }
 
+        wordList.add(word);
+        defList.add(definition);
+
+        currentIndex++;
+
         shownDef = false;
     }
 
-    private void showDefinition()
-    {
-        defText = (TextView) findViewById(R.id.textView2);
-        findViewById(R.id.textView).clearAnimation();
-        TranslateAnimation translation;
-        translation = new TranslateAnimation(0f, 0F, 0f, getDisplayHeight()-300 );
-        translation.setStartOffset(0);
-        translation.setDuration(500);
-        translation.setFillAfter(true);
-        translation.setInterpolator(new DecelerateInterpolator());
-        findViewById(R.id.textView).startAnimation(translation);
-        Animation in = new AlphaAnimation(0.0f, 1.0f);
-        in.setDuration(500);
-        defText.setText(definition);
-        defText.startAnimation(in);
-        shownDef = true;
 
-
-
-    }
 
     private int getDisplayHeight() {
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         return metrics.widthPixels;
     }
-    }
+}
 
 
 
